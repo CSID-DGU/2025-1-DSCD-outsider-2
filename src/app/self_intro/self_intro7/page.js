25590f41f227430afc999e4300e45759f29f8878
yoojeongping <xhddurdl@naver.com>
2025-05-23 14:46:02 +0900

    자기소개1~9 홈버튼 추가

--- src/app/self_intro/self_intro7/page.js
+++ src/app/self_intro/self_intro7/page.js
@@ -21,12 +21,19 @@ export default function SelfIntro7() {
     <div className="w-full min-h-screen bg-white relative">
       {/* 상단바 */}
       <div className="w-full px-14 py-5 flex justify-between items-center border-b border-gray-300">
-        <div className="text-black text-3xl font-bold">슈끌림</div>
-        <div className="flex gap-12 text-black text-base">
-          <button onClick={() => alert("사이트 소개 이동")}>사이트 소개</button>
-          <button onClick={() => alert("이용 방법 이동")}>이용 방법</button>
-          <button onClick={() => router.push("/mypage")}>마이페이지</button>
-          <button onClick={() => alert("장소 추천 이동")}>장소 추천</button>
+        <button onClick={() => router.push("/")} className="text-black text-3xl font-bold cursor-pointer">슈끌림</button>
+        <div className="flex gap-12">
+          <button onClick={() => router.push("/site_intro")} className="text-black text-base">사이트 소개</button>
+          <button onClick={() => router.push("/how_to_use")} className="text-black text-base">이용 방법</button>
+          <button onClick={() => {
+            const kakaoId = localStorage.getItem("kakaoId");
+            if (kakaoId) router.push("/mypage");
+            else {
+              alert("로그인이 필요합니다!");
+              router.push("/login");
+            }
+          }} className="text-black text-base">마이페이지</button>
+          <button onClick={() => router.push("/place_recommend")} className="text-black text-base">장소 추천</button>
         </div>
       </div>
 
